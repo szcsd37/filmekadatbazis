dd0fc572dbe166bf057cfdba60754a3e30752895
szcsd37 <szucs.dominik@diak.szbi-pg.hu>
2026-01-29 13:57:38 +0100

    komitolom

--- gyakorlo.sql
+++ gyakorlo.sql
@@ -15,3 +15,55 @@ SELECT * FROM filmek
 -- Keresd meg azokat a filmeket, amelyek 2000 után jelentek meg!
 
 SELECT * FROM filmek WHERE megjelenesi_ev > 2000
+
+-- 5. feladat
+-- Listázd ki azokat a filmeket, amelyek hossza 120 percnél hosszabb!
+
+
+
+-- 6. feladat
+-- Mutasd meg azokat a színészeket, akik aktívak (aktiv = 'igen')!
+
+
+-- 7. feladat
+-- Keresd meg azokat a filmeket, amelyek értékelése 8.0 felett van!
+
+
+-- 8. feladat
+-- Listázd ki azokat a filmeket, amelyek korhatára 16 vagy 18 év!
+
+
+-- Rendezés ORDER BY használatával
+
+-- 9. feladat
+-- Rendezd az összes filmet megjelenési év szerint növekvő sorrendben!
+
+
+-- 10. feladat
+-- Rendezd a filmeket értékelés szerint csökkenő sorrendben!
+
+SELECT * FROM filmek
+ORDER BY ertekeles DESC
+
+-- 11. feladat
+-- Listázd ki a színészeket születési dátum szerint növekvő sorrendben!
+
+SELECT * FROM szineszek
+ORDER BY szuletesi_datum ASC
+
+-- Összetett feltételek
+
+-- 12. feladat
+-- Keresd meg azokat a filmeket, amelyek 2010 és 2020 között jelentek meg ÉS értékelésük 7.5 felett van!
+
+SELECT * FROM filmek WHERE megjelenesi_ev BETWEEN 2010 AND 2020 AND ertekeles > 7.5
+
+-- 13. feladat
+-- Mutasd meg azokat a filmeket, amelyek hossza 90 és 120 perc között van!
+
+SELECT * FROM filmek WHERE hossz BETWEEN 90 AND 120
+
+-- 14. feladat
+-- Listázd ki azokat a színészeket, akik nem magyar nemzetiségűek ÉS aktívak!
+
+SELECT * FROM szineszek WHERE nemzetiseg != 'magyar' AND aktiv = 'igen'
---
+++ insertdeletestb.sql
@@ -0,0 +1,57 @@
+
+-- 1.
+
+INSERT into filmek
+(cim, rendezo,, megjelenesi_ev, hossz, korhatar, ertekeles)
+VALUES ("Toy Story 4", "Josh Cooley", 2019, 100, 0, 8.7)
+
+-- 2.
+
+INSERT into szineszek
+(nev, szuletesi_datum, nemzetiseg, aktiv)
+VALUES ("Ka Kálmán", 1877, "Hódmezővásárhelyi", "igen")
+VALUES ("Justin Zalán", 1984, "Német", "nem")
+
+-- 3.
+
+INSERT into filmek
+(cim, rendezo, megjelenesi_ev, hossz, korhatar, ertekeles)
+VALUES ("Márkó's Takeover", "Jaksa Márkó", 2019, 300, 18, 8.7)
+
+-- 4.
+
+UPDATE filmek
+SET rendezo = "Joe Ranft"
+WHERE cim = "Toy Story"
+
+-- 5.
+
+UPDATE filmek
+SET megjelenesi_ev = 1999
+WHERE cim = "Matrix"
+
+-- 6.
+
+UPDATE filmek
+SET ertekeles = 8.9, cim = "A tégla(felújított kiadás)"
+WHERE cim = "A tégla"
+
+-- 7.
+UPDATE szineszek
+SET aktiv = "igen", nemzetiseg = "brit"
+WHERE nev = "Daniel Day-Lewis"
+
+-- 8.
+
+DELETE from filmek
+WHERE megjelenesi_ev < 1990
+
+-- 9.
+
+DELETE FROM filmek
+WHERE aktiv = "nem"
+
+-- 10.
+
+DELETE FROM filmek
+WHERE cim = "Toy Story 4"
